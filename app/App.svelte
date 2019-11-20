8192a03800da2b7ef10453bbfdb41140fb52f4ed
Sam Shores <plaidphantom@gmail.com>
2019-11-20 12:51:47 -0600

    init

---
+++ app/App.svelte
@@ -0,0 +1,42 @@
+<page>
+    <actionBar title="Svelte Native App" />
+    <stackLayout>
+        <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
+            <formattedString>
+                <span class="fas" text="&#xf135;" />
+                <span text=" {message}" />
+            </formattedString>
+        </label>
+        <label text="val: {val}" />
+        <button text="Go to Other" on:tap="{go}" />
+    </stackLayout>
+</page>
+
+<script>
+    import { navigate, showModal } from 'svelte-native';
+    import { onMount } from 'svelte';
+
+    import Other from './Other.svelte';
+
+    let val = 0;
+    let message = "Blank Svelte Native App"
+
+    async function go() {
+        val = await showModal({page: Other, stretched: false });
+    }
+
+    onMount(async () => {
+        //setTimeout(async () => { await go(); }, 500);
+        await go();
+    });
+</script>
+
+<style>
+    .info .fas {
+        color: #3A53FF;
+    }
+
+    .info {
+        font-size: 20;
+    }
+</style>
---
+++ app/Other.svelte
@@ -0,0 +1,30 @@
+<page>
+    <actionBar title="Svelte Native App" />
+    <stackLayout>
+        <label class="info" horizontalAlignment="center" verticalAlignment="center" textWrap="true">
+            <formattedString>
+                <span class="fas" text="&#xf135;" />
+                <span text=" The other page!" />
+            </formattedString>
+        </label>
+        <button text="Go Back" on:tap="{back}" />
+    </stackLayout>
+</page>
+
+<script>
+    import { closeModal } from 'svelte-native';
+
+    function back() {
+        closeModal('other');
+    }
+</script>
+
+<style>
+    .info .fas {
+        color: #3A53FF;
+    }
+
+    .info {
+        font-size: 20;
+    }
+</style>
